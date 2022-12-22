a15cb2247820361568f55455d145ae5e62f484c4
LucaArcudi <arcudilu@gmail.com>
2022-12-22 10:33:25 +0100

    ..

--- JS/script.js
+++ JS/script.js
@@ -19,7 +19,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      oddNumbers : [],
+      evenNumbers : []
     }
   }
 }).mount('#app')
---
+++ snack-1/JS/script.js
@@ -0,0 +1,16 @@
+/***
+Chiedere all'API 10 nomi da inserire in un array di invitati.
+ Una volta generata la lista chiedere all'utente di dire il proprio nome.
+ Se è uno degli invitati ritornare un messaggio di benvenuto, altrimenti di accesso negato.
+*/
+
+
+const {createApp} = Vue;
+
+createApp ({
+    data() {
+        return {
+           guests : [] 
+        }
+    }    
+}).mount("#app")
